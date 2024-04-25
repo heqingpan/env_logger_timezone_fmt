a48108e538c261f3a5323d72433a73a0b02e81ae
heqingpan <heqingpan@126.com>
2024-04-25 23:16:16 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+
+const datatime_fmt_second: &'static str = "%Y-%m-%d %H:%M:%S% %:z";
+const datatime_fmt_3f: &'static str = "%Y-%m-%d %H:%M:%S%.3f %:z";
+const datatime_fmt_6f: &'static str = "%Y-%m-%d %H:%M:%S%.6f %:z";
